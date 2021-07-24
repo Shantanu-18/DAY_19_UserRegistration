a13c30cb2c01e668e5bec9f280279922d4c4ff88
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-24 16:10:39 +0530

    [Shantanu] Added : Main program file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DAY_19_UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to user registration!");
+
+            string FirstName;
+            Console.Write("Enter First Name: ");
+            FirstName = Console.ReadLine();
+
+            UserRegistration.checkFirstName(FirstName);
+
+
+
+
+        }
+    }
+}
