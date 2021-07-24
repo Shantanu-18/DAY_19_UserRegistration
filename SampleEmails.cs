45c5b6390f4fd33547f9bd86cb41595c481d5546
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-24 21:29:33 +0530

    [Shantanu] Added : Checking validation of given sample emails

---
+++ SampleEmails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAY_19_UserRegistration
+{
+    public class SampleEmails
+    {
+        public static string Regex_SampleEmails = "^[A-Za-z]+([-.+#^*][0-9]+)?[0-9]*[@][A-Za-z0-9]+[.][A-Za-z]{2,}([.][a-zA-Z]{2,})?$";
+
+        public static bool emailValide(string emailSample)
+        {
+            return Regex.IsMatch(emailSample, Regex_SampleEmails);
+        }
+    }
+}
