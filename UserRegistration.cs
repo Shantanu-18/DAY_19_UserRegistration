2b4020dec12bd6a878c807addf9f6eb08896ee5c
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-28 21:18:59 +0530

    [Shantanu] Added : Regex for user registration

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -11,76 +11,36 @@ namespace DAY_19_UserRegistration
         public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
         public static string Regex_Email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
         public static string Regex_Number = "^[0-9]{2}[ ][6-9][0-9]{9}$";
-        public static string Regex_Password = "^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))";
+        public static string Regex_Password = "^((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))";
 
 
-        public static bool ValidateFirstName(string FirstName)
+        public bool ValidateFirstName(string FirstName)
         {
             return Regex.IsMatch(FirstName, Regex_FirstName);
         }
 
 
-        public static void checkFirstName(string FirstName)
-        {
-            if (Regex.IsMatch(FirstName, Regex_FirstName) == false)
-            {
-                Console.WriteLine("Enter valid First name. It should Start with capital letters and minimum 3 letters!");
-            }
-        }
-
-
-        public static bool ValidateLastName(string LastName)
+        public bool ValidateLastName(string LastName)
         {
             return Regex.IsMatch(LastName, Regex_LastName);
         }
+       
 
-        public static void checkLastName(string LastName)
-        {
-            if (Regex.IsMatch(LastName, Regex_LastName) == false)
-            {
-                Console.WriteLine("Enter valid Last name. It should Start with capital letters and minimum 3 letters!");
-            }
-        }
-
-        public static bool validateEmail(string email)
+        public bool validateEmail(string email)
         {
             return Regex.IsMatch(email, Regex_Email);
         }
+       
 
-        public static void checkemail(string email)
-        {
-            if (Regex.IsMatch(email, Regex_Email) == false)
-            {
-                Console.WriteLine("Enter valid email address!");
-            }
-        }
-
-        public static bool validatenumber(string number)
+        public bool validatenumber(string number)
         {
             return Regex.IsMatch(number, Regex_Number);
         }
+       
 
-        public static void checknumber(string number)
-        {
-            if (Regex.IsMatch(number, Regex_Number) == false)
-            {
-                Console.WriteLine("Enter valid number!");
-            }
-        }
-
-        public static bool validatePassword(string password)
+        public bool validatePassword(string password)
         {
             return Regex.IsMatch(password, Regex_Password);
         }
-
-        public static void checkPassword(string password)
-        {
-            if (Regex.IsMatch(password, Regex_Password) == false)
-            {
-                Console.WriteLine("Enter valid password. It should contain minimum 8 character," +
-                    "atleast one upper case character and atleast one numeric value and atleast one special character");
-            }
-        }
-
     }
 }
