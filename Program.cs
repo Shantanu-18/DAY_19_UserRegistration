68b72e4e8a511d344e5377fcc18af1f990e8f7e2
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-28 21:18:04 +0530

    [Shantanu] Added : Main program file for user registration

--- Program.cs
+++ Program.cs
@@ -8,29 +8,29 @@ namespace DAY_19_UserRegistration
         {
             Console.WriteLine("Welcome to user registration!");
 
-            string FirstName, LastName, email, number, password;
-            Console.Write("Enter First Name: ");
-            FirstName = Console.ReadLine();
-            UserRegistration.checkFirstName(FirstName);
+            //string FirstName, LastName, email, number, password;
+            //Console.Write("Enter First Name: ");
+            //FirstName = Console.ReadLine();
+            //UserRegistration.checkFirstName(FirstName);
 
-            Console.Write("Enter Last Name: ");
-            LastName = Console.ReadLine();
-            UserRegistration.checkLastName(LastName);
+            //Console.Write("Enter Last Name: ");
+            //LastName = Console.ReadLine();
+            //UserRegistration.checkLastName(LastName);
 
-            Console.Write("Enter your email addresss: ");
-            email = Console.ReadLine();
-            UserRegistration.checkemail(email);
+            //Console.Write("Enter your email addresss: ");
+            //email = Console.ReadLine();
+            //UserRegistration.checkemail(email);
 
-            Console.Write("Enter your mobile number: ");
-            number = Console.ReadLine();
-            UserRegistration.checknumber(number);
+            //Console.Write("Enter your mobile number: ");
+            //number = Console.ReadLine();
+            //UserRegistration.checknumber(number);
 
-            Console.Write("Enter your password: ");
-            password = Console.ReadLine();
-            UserRegistration.checkPassword(password);
+            //Console.Write("Enter your password: ");
+            //password = Console.ReadLine();
+            //UserRegistration.checkPassword(password);
 
 
-            Console.WriteLine(SampleEmails.emailValide("abc+100@gmail.com.com"));
+            //Console.WriteLine(SampleEmails.emailValide("abc+100@gmail.com.com"));
         }
     }
 }
