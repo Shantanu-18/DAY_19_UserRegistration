f205a0d4de5c47e51ea4ebfddc2d1ac52b87c223
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-31 13:42:34 +0530

    [Shantanu] Added : Custom exception handling for Sample emails check

--- SampleEmails.cs
+++ SampleEmails.cs
@@ -9,8 +9,30 @@ namespace DAY_19_UserRegistration
     {
         public static string Regex_SampleEmails = "^[A-Za-z]+([-.+#^*][0-9]+)?[0-9]*[@][A-Za-z0-9]+[.][A-Za-z]{2,}([.][a-zA-Z]{2,})?$";
 
-        public static bool emailValide(string emailSample)
+        public bool emailValidate(string emailSample)
         {
+            var result = Regex.IsMatch(emailSample, Regex_SampleEmails);
+            try
+            {
+                if (result == false)
+                {
+                    if (emailSample == string.Empty || emailSample == " ")
+                    {
+
+                        throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.NULL_MESSAGE,
+                                "Enter email address it should not be blank.");
+                    }
+                    else if (result == false)
+                    {
+                        throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.INVALID_INPUT,
+                                "Enter valid email address.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             return Regex.IsMatch(emailSample, Regex_SampleEmails);
         }
     }
