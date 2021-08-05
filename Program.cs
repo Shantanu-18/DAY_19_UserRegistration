79f286a59b076042f495cfc9cecef97930733f46
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-05 23:41:31 +0530

    [Shantanu] Added : Main method and giving input.

--- Program.cs
+++ Program.cs
@@ -8,32 +8,36 @@ namespace DAY_19_UserRegistration
         {
             Console.WriteLine("Welcome to user registration!");
 
-            UserRegistration obj = new UserRegistration();
-            string FirstName, LastName, email, number, password;
+            //UserRegistration obj = new UserRegistration();
+            //string FirstName, LastName, email, number, password;
 
-            Console.Write("Enter First Name: ");
-            FirstName = Console.ReadLine();
-            obj.ValidateFirstName(FirstName);
+            //Console.Write("Enter First Name: ");
+            //FirstName = Console.ReadLine();
+            //obj.ValidateFirstName(FirstName);
 
-            Console.Write("Enter Last Name: ");
-            LastName = Console.ReadLine();
-            obj.ValidateLastName(LastName);
+            //Console.Write("Enter Last Name: ");
+            //LastName = Console.ReadLine();
+            //obj.ValidateLastName(LastName);
 
-            Console.Write("Enter your email addresss: ");
-            email = Console.ReadLine();
-            obj.validateEmail(email);
+            //Console.Write("Enter your email addresss: ");
+            //email = Console.ReadLine();
+            //obj.validateEmail(email);
 
-            Console.Write("Enter your mobile number: ");
-            number = Console.ReadLine();
-            obj.validatenumber(number);
+            //Console.Write("Enter your mobile number: ");
+            //number = Console.ReadLine();
+            //obj.validatenumber(number);
 
-            Console.Write("Enter your password: ");
-            password = Console.ReadLine();
-            obj.validatePassword(password);
+            //Console.Write("Enter your password: ");
+            //password = Console.ReadLine();
+            //obj.validatePassword(password);
 
-            SampleEmails objec = new SampleEmails();
-            objec.emailValidate("abc+100@gmail.com.com");
-            Console.WriteLine(objec.emailValidate("abc+100@gmail.com.com"));
+            //SampleEmails objec = new SampleEmails();
+            //objec.emailValidate("abc+100@gmail.com.com");
+            //Console.WriteLine(objec.emailValidate("abc+100@gmail.com.com"));
+
+            UsingLambdaExpression lambda = new UsingLambdaExpression();
+            lambda.ValidationUsingRegex();
+
         }
     }
 }
